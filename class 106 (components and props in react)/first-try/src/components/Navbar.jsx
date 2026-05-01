01e420fd92222375fd9766077e9a8d2322b8d24d
Muhammad-Shayan00 <shayanofficial456@gmail.com>
2026-05-01 12:15:59 +0500

    class 106 & 107 added

---
+++ class 106 (components and props in react)/first-try/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>about</li>
+                <li>contact</li>
+                <li>login</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default navbar
